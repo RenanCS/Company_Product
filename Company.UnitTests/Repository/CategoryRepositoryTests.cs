5c1c88d5cfdbe962c829b982146acd70bfb42968
renan <renan@interprocess.com.br>
2022-06-23 23:09:55 -0300

    feat: add tests repository

---
+++ Company.UnitTests/Repository/CategoryRepositoryTests.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using Bogus;
+using Company.Core.Entities;
+using Company.Core.Repositories;
+using Company.Infrastructure.Persistence;
+using Company.Infrastructure.Persistence.Repositories;
+using Company.UnitTests.Helper;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Company.UnitTests.Repository
+{
+    public class CategoryRepositoryTests
+    {
+        private ICategoryRepository _categoryRepository;
+
+ 
+        #region MAKESUT
+
+        private void MakeSut(DbContextOptions<CompanyDbContext> optionsStub)
+        {
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                context.Database.OpenConnection();
+                context.Database.EnsureCreated();
+            }
+        }
+        #endregion
+
+
+        [Fact]
+        public async Task GetAllAsync_ShoulReturnListWithFiveCategoies_WhenCategoriesExists()
+        {
+            var optionsStub = FactoryDbContext.CreateConnection();
+            MakeSut(optionsStub);
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                _categoryRepository = new CategoryRepository(context);
+
+                var result = await _categoryRepository.GetAllAsync();
+
+                Assert.Equal(5, result.Count());
+            }
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ShoulReturnZeroCategory_WhenCategoriesNotExists()
+        {
+            var optionsStub = FactoryDbContext.CreateConnection();
+            MakeSut(optionsStub);
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                var all = from c in context.Categories select c;
+                context.Categories.RemoveRange(all);
+                context.SaveChanges();
+            }
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                _categoryRepository = new CategoryRepository(context);
+
+                var result = await _categoryRepository.GetAllAsync();
+
+                Assert.Equal(0, result.Count());
+            }
+
+        }
+
+
+    }
+}
---
+++ Company.UnitTests/Repository/ProductRepositoryTests.cs
@@ -0,0 +1,186 @@
+﻿using Company.Core.Entities;
+using Company.Core.Repositories;
+using Company.Infrastructure.Persistence;
+using Company.Infrastructure.Persistence.Repositories;
+using Company.UnitTests.Helper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Company.UnitTests.Repository
+{
+    public class ProductRepositoryTests
+    {
+        private IProductRepository _productRepository;
+
+
+        #region MAKESUT
+
+        private void MakeSut(DbContextOptions<CompanyDbContext> optionsStub)
+        {
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                context.Database.OpenConnection();
+                context.Database.EnsureCreated();
+            }
+        }
+        #endregion
+
+
+        [Fact]
+        public async Task GetAllAsync_ShoulReturnListProducts_WhenProductExists()
+        {
+            var optionsStub = FactoryDbContext.CreateConnection();
+            var expectCountCategory = 5;
+           
+            MakeSut(optionsStub);
+
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                var categoryDb = context.Categories.First();
+                var productsStub = FactoryProduct.GetProductsFaker(expectCountCategory, categoryDb);
+                context.Products.AddRange(productsStub);
+                context.SaveChanges();
+            }
+
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                _productRepository = new ProductRepository(context);
+
+                var result = await _productRepository.GetAllAsync();
+
+                Assert.Equal(expectCountCategory, result.Count());
+            }
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ShoulReturnProduct_WhenIdProductExists()
+        {
+            var optionsStub = FactoryDbContext.CreateConnection();
+            Product productStub = null;
+
+            MakeSut(optionsStub);
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                var categoryDb = context.Categories.First();
+                productStub = FactoryProduct.GetProductsFaker(1, categoryDb).First();
+                context.Products.Add(productStub);
+                context.SaveChanges();
+            }
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                _productRepository = new ProductRepository(context);
+
+                var result = await _productRepository.GetByIdAsync(productStub.Id);
+
+                Assert.Equal(productStub.Id, result.Id);
+            }
+
+        }
+
+        [Fact]
+        public async Task CreateAsync_ShoulReturnProduct_WhenAddNewProduct()
+        {
+            var optionsStub = FactoryDbContext.CreateConnection();
+            Product productStub = null;
+
+            MakeSut(optionsStub);
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                var categoryDb = context.Categories.First();
+                productStub = FactoryProduct.GetProductsFaker(1, categoryDb).First();
+
+                _productRepository = new ProductRepository(context);
+                await _productRepository.CreateAsync(productStub);
+            }
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                _productRepository = new ProductRepository(context);
+
+                var result = await _productRepository.GetByIdAsync(productStub.Id);
+
+                Assert.Equal(productStub.Id, result.Id);
+            }
+        }
+
+        [Fact]
+        public async Task UpdateAsync_ShoulReturnProductDiff_WhenUpdatedProduct()
+        {
+            var optionsStub = FactoryDbContext.CreateConnection();
+            Product initialProductMock = null;
+            Product updatedProductMock = null;
+
+            MakeSut(optionsStub);
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                var categoryDb = context.Categories.First();
+                initialProductMock = FactoryProduct.GetProductsFaker(1, categoryDb).First();
+
+                _productRepository = new ProductRepository(context);
+                await _productRepository.CreateAsync(initialProductMock);
+            }
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                _productRepository = new ProductRepository(context);
+                updatedProductMock = await _productRepository.GetByIdAsync(initialProductMock.Id);
+                updatedProductMock.Description = "Teste Update";
+            }
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                _productRepository = new ProductRepository(context);
+               await _productRepository.UpdateAsync(updatedProductMock);                 
+            }
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                _productRepository = new ProductRepository(context);
+                var result = await _productRepository.GetByIdAsync(initialProductMock.Id);
+                Assert.NotEqual(initialProductMock.Description, result.Description);
+            }
+        }
+
+        [Fact]
+        public async Task DeleteAsyncc_ShoulReturnNull_WhenRemoveProduct()
+        {
+            var optionsStub = FactoryDbContext.CreateConnection();
+            Product initialProductMock = null;
+
+            MakeSut(optionsStub);
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                var categoryDb = context.Categories.First();
+                initialProductMock = FactoryProduct.GetProductsFaker(1, categoryDb).First();
+
+                _productRepository = new ProductRepository(context);
+                await _productRepository.CreateAsync(initialProductMock);
+            }                      
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                _productRepository = new ProductRepository(context);
+                await _productRepository.DeleteAsync(initialProductMock.Id);
+            }
+
+            using (var context = new CompanyDbContext(optionsStub))
+            {
+                _productRepository = new ProductRepository(context);
+                var result = await _productRepository.GetByIdAsync(initialProductMock.Id);
+                Assert.Null( result);
+            }
+        }
+    }
+}
