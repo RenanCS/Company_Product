f3c5e67dbfd4dfd1fdbe7d7ed20dfa471f3f0f8d
renan <renan@interprocess.com.br>
2022-06-23 23:09:32 -0300

    feat: add factory db context

---
+++ Company.UnitTests/Helper/FactoryDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Company.Infrastructure.Persistence;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Company.UnitTests.Helper
+{
+    public class FactoryDbContext
+    {
+        public static DbContextOptions<CompanyDbContext> CreateConnection()
+        {
+           
+                var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = ":memory:" };
+                var connection = new SqliteConnection(connectionStringBuilder.ToString());
+                var options = new DbContextOptionsBuilder<CompanyDbContext>().UseSqlite(connection).Options;
+                return options;
+        }
+
+    }
+}
