08c2e4933b3f49450f475a4bf99e7ece8a937268
Renan Carvalho <renan@interprocess.com.br>
2022-07-01 09:10:55 -0300

    chore: add class fixture for productservice

---
+++ Company.UnitTests/Fixtures/ProductServiceFixture.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Company.Application.Service;
+using Company.Application.Service.Interface;
+using Company.Core.Repositories;
+using Company.UnitTests.Helper;
+using Moq;
+
+namespace Company.UnitTests.Fixtures
+{
+    public class ProductServiceFixture
+    {
+        public IProductService _productService { get; private set; }
+        public Mock<IProductRepository> _productRepositoryMock { get; }
+        private IMapper _mapper;
+
+        public ProductServiceFixture()
+        {
+            _mapper = AutoMappingHelper.ConfigureAutoMapping();
+            _productRepositoryMock = new Mock<IProductRepository>();
+        }
+
+        public void MakeSut()
+        {
+            _productService = new ProductService(_productRepositoryMock.Object, _mapper);
+        }
+    }
+}
--- Company.UnitTests/Service/ProductServiceTests.cs
+++ Company.UnitTests/Service/ProductServiceTests.cs
@@ -5,6 +5,7 @@ using Company.Application.Service;
 using Company.Application.Service.Interface;
 using Company.Core.Entities;
 using Company.Core.Repositories;
+using Company.UnitTests.Fixtures;
 using Company.UnitTests.Helper;
 using Moq;
 using System.Collections.Generic;
@@ -14,43 +15,32 @@ using Xunit;
 
 namespace Company.UnitTests.Service
 {
-    public class ProductServiceTests
+    public class ProductServiceTests: IClassFixture<ProductServiceFixture>
     {
-        private IProductService _productService;
-        private Mock<IProductRepository> _productRepositoryMock;
+        private readonly ProductServiceFixture _productServiceFixture;
         private Faker _faker;
-        private IMapper _mapper;
 
 
-        public ProductServiceTests()
+        public ProductServiceTests(ProductServiceFixture productServiceFixture)
         {
+            _productServiceFixture = productServiceFixture;
             _faker = new Faker();
-            _productRepositoryMock = new Mock<IProductRepository>();
-            _mapper = AutoMappingHelper.ConfigureAutoMapping();
 
         }
-
-
-        #region MAKESUT
-
-        private IProductService MakeSut(IProductRepository stubProductRepository)
-        {
-            var stubService = new ProductService(stubProductRepository, _mapper);
-
-            return stubService;
-        }
-        #endregion
 
         [Fact]
         public async Task GetProductsAsync_ShoulReturnList_WhenProductExists()
         {
             IEnumerable<Product> productsStub = FactoryProduct.GetProductsFaker();
 
-            _productRepositoryMock.Setup(x => x.GetAllAsync()).ReturnsAsync(productsStub);
+            _productServiceFixture
+                ._productRepositoryMock
+                .Setup(x => x.GetAllAsync())
+                .ReturnsAsync(productsStub);
 
-            _productService = MakeSut(_productRepositoryMock.Object);
+            _productServiceFixture.MakeSut();
 
-            var result = await _productService.GetProductsAsync();
+            var result = await _productServiceFixture._productService.GetProductsAsync();
 
             Assert.Equal(productsStub.Count(), result.Count());
         }
@@ -60,11 +50,14 @@ namespace Company.UnitTests.Service
         {
             var productsStub = new Product[] { };
 
-            _productRepositoryMock.Setup(x => x.GetAllAsync()).ReturnsAsync(productsStub);
+            _productServiceFixture
+               ._productRepositoryMock
+               .Setup(x => x.GetAllAsync())
+               .ReturnsAsync(productsStub);
 
-            _productService = MakeSut(_productRepositoryMock.Object);
+             _productServiceFixture.MakeSut();
 
-            var result = await _productService.GetProductsAsync();
+            var result = await _productServiceFixture._productService.GetProductsAsync();
 
             Assert.Empty(result);
         }
@@ -74,11 +67,14 @@ namespace Company.UnitTests.Service
         {
             Product productsStub = FactoryProduct.GetProductsFaker().FirstOrDefault();
 
-            _productRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(productsStub);
+            _productServiceFixture
+                ._productRepositoryMock
+                .Setup(x => x.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(productsStub);
 
-            _productService = MakeSut(_productRepositoryMock.Object);
+            _productServiceFixture.MakeSut();
 
-            var result = await _productService.GetProductByIdAsync(productsStub.Id);
+            var result = await _productServiceFixture._productService.GetProductByIdAsync(productsStub.Id);
 
             Assert.NotNull(result);
         }
@@ -88,11 +84,14 @@ namespace Company.UnitTests.Service
         {
             Product productsStub = null;
 
-            _productRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(productsStub);
+            _productServiceFixture.
+                _productRepositoryMock
+                .Setup(x => x.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(productsStub);
 
-            _productService = MakeSut(_productRepositoryMock.Object);
+            _productServiceFixture.MakeSut();
 
-            var result = await _productService.GetProductByIdAsync(_faker.Random.Int(1, 10));
+            var result = await _productServiceFixture._productService.GetProductByIdAsync(_faker.Random.Int(1, 10));
 
             Assert.Null(result);
         }
@@ -102,11 +101,13 @@ namespace Company.UnitTests.Service
         {
             ProductInputModel productInputModelStub = FactoryProduct.GetProductInputModelFaker(_faker.Random.Int(1, 10));
 
-            _productRepositoryMock.Setup(x => x.CreateAsync(It.IsAny<Product>()));
+            _productServiceFixture
+                ._productRepositoryMock
+                .Setup(x => x.CreateAsync(It.IsAny<Product>()));
 
-            _productService = MakeSut(_productRepositoryMock.Object);
+            _productServiceFixture.MakeSut();
 
-            var result = await _productService.AddProductAsync(productInputModelStub);
+            var result = await _productServiceFixture._productService.AddProductAsync(productInputModelStub);
 
             Assert.True(result);
         }
@@ -116,9 +117,9 @@ namespace Company.UnitTests.Service
         {
             ProductInputModel productInputModelStub = null;
 
-            _productService = MakeSut(_productRepositoryMock.Object);
+            _productServiceFixture.MakeSut();
 
-            var result = await _productService.AddProductAsync(productInputModelStub);
+            var result = await _productServiceFixture._productService.AddProductAsync(productInputModelStub);
 
             Assert.False(result);
         }
@@ -128,11 +129,13 @@ namespace Company.UnitTests.Service
         {
             ProductInputModel productInputModelStub = FactoryProduct.GetProductInputModelFaker(_faker.Random.Int(1, 10));
 
-            _productRepositoryMock.Setup(x => x.CreateAsync(It.IsAny<Product>()));
+            _productServiceFixture
+                ._productRepositoryMock
+                .Setup(x => x.CreateAsync(It.IsAny<Product>()));
 
-            _productService = MakeSut(_productRepositoryMock.Object);
+            _productServiceFixture.MakeSut();
 
-            var result = await _productService.UpdateProductyAsync(productInputModelStub);
+            var result = await _productServiceFixture._productService.UpdateProductyAsync(productInputModelStub);
 
             Assert.True(result);
         }
@@ -142,9 +145,9 @@ namespace Company.UnitTests.Service
         {
             ProductInputModel productInputModelStub = null;
 
-            _productService = MakeSut(_productRepositoryMock.Object);
+            _productServiceFixture.MakeSut();
 
-            var result = await _productService.UpdateProductyAsync(productInputModelStub);
+            var result = await _productServiceFixture._productService.UpdateProductyAsync(productInputModelStub);
 
             Assert.False(result);
         }
@@ -154,13 +157,13 @@ namespace Company.UnitTests.Service
         {
             Product productsStub = FactoryProduct.GetProductsFaker().FirstOrDefault();
 
-            _productRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(productsStub);
+            _productServiceFixture._productRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(productsStub);
 
-            _productRepositoryMock.Setup(x => x.DeleteAsync(It.IsAny<int>()));
+            _productServiceFixture._productRepositoryMock.Setup(x => x.DeleteAsync(It.IsAny<int>()));
 
-            _productService = MakeSut(_productRepositoryMock.Object);
+            _productServiceFixture.MakeSut();
 
-            var result = await _productService.RemoveProductAsync(_faker.Random.Int(1, 10));
+            var result = await _productServiceFixture._productService.RemoveProductAsync(_faker.Random.Int(1, 10));
 
             Assert.True(result);
         }
@@ -168,9 +171,9 @@ namespace Company.UnitTests.Service
         [Fact]
         public async Task RemoveProductAsync_ShoulReturnFalse_WheIdProductEqualsZero()
         {
-            _productService = MakeSut(_productRepositoryMock.Object);
+            _productServiceFixture.MakeSut();
 
-            var result = await _productService.RemoveProductAsync(0);
+            var result = await _productServiceFixture._productService.RemoveProductAsync(0);
 
             Assert.False(result);
         }
@@ -180,11 +183,11 @@ namespace Company.UnitTests.Service
         {
             Product productsStub = null;
 
-            _productRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(productsStub);
+            _productServiceFixture._productRepositoryMock.Setup(x => x.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(productsStub);
 
-            _productService = MakeSut(_productRepositoryMock.Object);
+            _productServiceFixture.MakeSut();
 
-            var result = await _productService.RemoveProductAsync(_faker.Random.Int(1,10));
+            var result = await _productServiceFixture._productService.RemoveProductAsync(_faker.Random.Int(1,10));
 
             Assert.False(result);
         }
